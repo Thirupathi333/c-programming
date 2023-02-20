cc87562e9e33eed6fa1324e224824a73b1b13e22
Thirupathi333 <123434039+Thirupathi333@users.noreply.github.com>
2023-02-20 06:42:52 +0000

    Create 38.c

---
+++ 38.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,i,n;
+	printf("enter the n value");
+	scanf("%d",&n);
+	a=1;
+	b=0;
+	for(i=0;i<n;i++)
+	{ printf("%d",c);
+	c=a+b;
+	a=b;
+	b=c;
+	}
+	return 0;
+}
