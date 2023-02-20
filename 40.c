b95bf6376491448fb695a3ba870e871442eef05b
Thirupathi333 <123434039+Thirupathi333@users.noreply.github.com>
2023-02-20 06:44:48 +0000

    Create 40.c

---
+++ 40.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main() {
+  int max; /* max value of side a or b */
+  int a, b; /* two sides of a triangle */
+  printf("Enter max value of sides a, b: ");
+  scanf("%d", &max);
+  for (a = 1; a <= max; a++) {
+    for (b = a; b <= max; b++) {
+      float c = sqrt(a*a+ b * b); /*third side*/
+      if (c == (int) c)
+        printf ("%2d %2d %2d\n", a, b, (int) c);
+    }
+  }
+   getch();
+}
